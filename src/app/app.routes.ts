40a3668625928994faea321e9530548c7b945035
CMChavez <aerocmc@gmail.com>
2025-04-09 13:18:00 -0300

    Fix: Lazy loading HeroList. Styles: Background colors and HeroForm.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,19 @@ import { HeroListComponent } from './components/layout/hero-list/hero-list.compo
 import { HeroFormComponent } from './components/layout/hero-form/hero-form.component';
 
 export const routes: Routes = [
-  {path: '', component: HeroListComponent},
-  {path: 'add', component: HeroFormComponent},
-  { path: 'edit/:heroId', loadComponent: () => import('./components/layout/hero-form/hero-form.component').then(m => m.HeroFormComponent) },
+  { path: '', component: HeroListComponent },
+  {
+    path: 'add',
+    loadComponent: () =>
+      import('./components/layout/hero-list/hero-list.component').then(
+        (m) => m.HeroListComponent
+      ),
+  },
+  {
+    path: 'edit/:heroId',
+    loadComponent: () =>
+      import('./components/layout/hero-form/hero-form.component').then(
+        (m) => m.HeroFormComponent
+      ),
+  },
 ];
