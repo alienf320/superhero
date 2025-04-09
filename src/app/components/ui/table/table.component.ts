7caab30f0df5147a06e86f55fa71e31d47b371cf
CMChavez <aerocmc@gmail.com>
2025-04-09 12:10:15 -0300

    Added: Font Awesome for icons: Style: Table icons.

--- src/app/components/ui/table/table.component.ts
+++ src/app/components/ui/table/table.component.ts
@@ -1,9 +1,11 @@
 import { Component, input, output } from '@angular/core';
 import { Hero } from '../../../shared/models/hero.model';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-table',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
@@ -11,4 +13,6 @@ export class TableComponent {
   heroes = input.required<Hero[]>();
   edit = output<number>();
   delete = output<number>();
+  faTrash = faTrash;
+  faPenToSquare = faPenToSquare;
 }
