9ac61586c3181c85101984eaaaf3b602e8fa25e7
CMChavez <aerocmc@gmail.com>
2025-04-09 21:38:48 -0300

    Testing: HeroList component y HeroService. Style: Paginator cambió a botones más descriptivos.

--- src/app/components/ui/paginator/paginator.component.ts
+++ src/app/components/ui/paginator/paginator.component.ts
@@ -4,26 +4,40 @@ import { Component, computed, input, output, signal } from '@angular/core';
   selector: 'app-paginator',
   imports: [],
   templateUrl: './paginator.component.html',
-  styleUrl: './paginator.component.scss'
+  styleUrl: './paginator.component.scss',
 })
 export class PaginatorComponent {
   total = input.required<number>();
   pageSize = input.required<number>();
   pageIndex = signal(0);
-  totalPages = computed(() => Math.ceil(this.total() / this.pageSize()))
+  totalPages = computed(() => Math.ceil(this.total() / this.pageSize()));
   pageChange = output<number>();
 
-  previous() {
-    if(this.pageIndex() > 0) {
-      this.pageIndex.update(i => i - 1);
-      this.pageChange.emit(this.pageIndex());
-    }
+  firstPage() {
+    this.setPage(0);
   }
 
-  next() {
-    if(this.pageIndex() < this.totalPages() -1) {
-      this.pageIndex.update(i => i + 1);
-      this.pageChange.emit(this.pageIndex())
+  lastPage() {
+    this.setPage(this.totalPages() - 1);
+  }
+
+  setPage(index: number) {
+    this.pageIndex.set(index);
+    this.pageChange.emit(index);
+  }
+
+  getPageRange(): number[] {
+    const current = this.pageIndex();
+    const total = this.totalPages();
+    const range: number[] = [];
+
+    const start = Math.max(0, current - 1);
+    const end = Math.min(total, current + 2);
+
+    for (let i = start; i < end; i++) {
+      range.push(i);
     }
+
+    return range;
   }
 }
